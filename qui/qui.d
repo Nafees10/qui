e67580fe77e329ea9060e58f91bf046ffa8c7d71
Nafees10 <nafees.hassan@outlook.com>
2017-03-22 16:01:16 +0500

    Most of the stuff is complete except for: adding widgets to owner/layouts, and sizing the widgets to fit into the layouts

--- qui/qui.d
+++ qui/qui.d
@@ -1,12 +1,20 @@
 ﻿module qui;
 
 import misc;
+import lists;
 import arsd.terminal;
 
 //MouseClick event
 struct MousePosition{
-	uinteger x;
-	uinteger y;
+	enum Button{
+		Left,
+		ScrollUp,
+		ScrollDown,
+		Right
+	}
+	Button mouseButton;
+	int x;
+	int y;
 }
 
 //Key press event
@@ -43,13 +51,9 @@ struct KeyPress{
 }
 
 //same as termial.RGB, but using this, no need to `import terminal;` just for one struct
-struct Color{
-	ubyte r, g, b;
-	//not used but required for compatibility with RGB
-	ubyte a;
-}
+alias RGBColor = RGB;
 
-
+//Vertical Alignment for layout
 enum HorizontalAlign{
 	Top,
 	Auto,
@@ -63,102 +67,232 @@ enum VerticalAlign{
 	Fill,
 	Right
 }
-//To specify alignment in QWidget
+///To specify alignment in QWidget
 union Align{
 	VerticalAlign vAlign;
 	HorizontalAlign hAlign;
+}
+
+//Position
+struct Position{
+	int x, y;
+}
+struct Size{
+	uinteger width, height;
+	uinteger maxWidth, maxHeight;
+	uinteger minWidth, minHeight;
+}
+//Cell (these will make up the entire terminal display)
+struct Cell{
+	char c;
+	RGBColor textColor;
+	RGBColor bgColor;
 }
 
 
 //base class for all widgets, including layouts
 abstract class QWidget{
 private:
-	uinteger x = 0, y = 0;
-	uinteger width, height, maxWidth, maxHeight, minWidth, minHeight;//0 = no limit
-	string captionText;
+	Position widgetPosition;
+	Size widgetSize;
+	string widgetCaption;
+	bool widgetShow = true;
 
-	//so Widget can send updateTerminal
-	QLayout* ownerLayout;
+	//so Widget can call update
+	QLayout* owner;
 public:
 	//constructor, to define owner and ownerTerminal
-	this(QLayout* layout){
-		ownerLayout = layout;
+	this(QLayout* ownerLayout){
+		owner = ownerLayout;
 	}
-	//called when mouse is clicked with cursor on widget
-	void onClick(MousePosition position){
+	//called by owner when mouse is clicked with cursor on widget
+	abstract void onClick(MousePosition cursorPos);
+	///called by owner when widget is selected and a key is pressed
+	abstract void onKeyPress(KeyPress key);
+	///Called by owner when the terminal is updating, use this to update the widget
+	abstract void update(ref Matrix display);
 
+	//properties:
+	@property string caption(){
+		return widgetCaption;
 	}
-	//called when widget is selected and a key is pressed
-	void onKeyPress(KeyPress key){
-
+	@property string caption(string newCaption){
+		return widgetCaption = newCaption;
 	}
-	//to get X axis position of widget, property setter can be set later, as this is an abstract class
-	@property uinteger positionX(){ return x; }
-	//to get Y axis position of widget, property setter can be set later, as this is an abstract class
-	@property uinteger positionY(){ return y; }
-	@property string caption(){ return captionText; }
 
+	@property Position position(){
+		return widgetPosition;
+	}
+	@property Position position(Position newPosition){
+		return widgetPosition = newPosition;
+	}
+
+	@property Size size(){
+		return widgetSize;
+	}
+	@property Size size(Size newSize){
+		return widgetSize = newSize;
+	}
 }
 
 //to contain widgets in an order
 class QLayout : QWidget{
 private:
 	QWidget[] widgetList;
-	Terminal* ownerTerminal;
+	QWidget* activeWidget;
+	QTerminal* ownerTerminal;
 	QLayout* ownerLayout;
 	LayoutDisplayType layoutType;
 public:
 	enum LayoutDisplayType{
 		Vertical,
 		Horizontal,
-		Cellular
 	}
-	this(QLayout* layout, LayoutDisplayType type){
-		super(layout);
+	this(LayoutDisplayType type, QLayout* owner = null){
+		super(owner);
 		layoutType = type;
 	}
-	void update(/*QWidget caller*/){
-		//update self
 
-	}
-	//Functions for childWidgets to call to write
-	void writeChars(char[] c){
-		if (ownerTerminal){
+	void addWidget(QWidget widget){
+		if (layoutType == LayoutDisplayType.Vertical){
+
+		}else if (layoutType == LayoutDisplayType.Horizontal){
 
 		}
+	}
+
+	void onClick(MousePosition cursorPos){
+		//check on which widget the cursor was on
+		Position p;
+		Size s;
+		uinteger i;
+		QWidget widget;
+		for (i = 0; i < widgetList.length; i++){
+			widget = widgetList[i];
+			p = widget.position;
+			s = widget.size;
+			//check x-axis
+			if (cursorPos.x >= p.x && cursorPos.x < p.x + s.width){
+				//check y-axis
+				if (cursorPos.y >= p.y && cursorPos.y < p.y + s.height){
+					//then this is the widget
+					//mark this widget as active
+					activeWidget = &widget;
+					//and call onClick
+					widget.onClick(cursorPos);
+					break;
+				}
+			}
+		}
+	}
+	void onKeyPress(KeyPress key){
+		//check active widget, call onKeyPress
+		if (activeWidget !is null){
+			activeWidget.onKeyPress(key);
+		}
+	}
+	void update(ref Matrix display){
 
 	}
 }
 
-class QTerminal{
+class QTerminal : QLayout{
 private:
-	Terminal* terminal;
-	RealTimeConsoleInput* input;
-
-	QWidget[] widgetList;
-	uinteger height, width;
-	string captionText;
+	Terminal terminal;
+	RealTimeConsoleInput input;
+	bool isRunning = false;
+	//QWidget[] widgetList;
 public:
-	this(string caption = "QUI Text User Interface"){
+	this(string caption = "QUI Text User Interface", LayoutDisplayType displayType = LayoutDisplayType.Vertical){
+		super(displayType, null);
 		//create terminal & input
-		*terminal = Terminal(ConsoleOutputType.cellular);
-		*input = RealTimeConsoleInput(terminal, ConsoleInputFlags.allInputEvents);
+		terminal = Terminal(ConsoleOutputType.cellular);
+		input = RealTimeConsoleInput(&terminal, ConsoleInputFlags.allInputEvents);
 		//init vars
-		height = terminal.height;
-		width = terminal.width;
-		captionText = caption;
+		widgetSize.height = terminal.height;
+		widgetSize.width = terminal.width;
+		widgetCaption = caption;
+		//set caption
+		terminal.setTitle(widgetCaption);
 	}
 	~this(){
 		terminal.clear;
 	}
 
+	void run(){
+		InputEvent event;
+		isRunning = true;
+		while (isRunning){
+			event = input.nextEvent;
+			//check event type
+			if (event.type == event.Type.KeyboardEvent){
+				KeyPress kPress;
+				kPress.key = event.get!(event.Type.KeyboardEvent).which;
+				this.onKeyPress(kPress);
+			}else if (event.type == event.Type.MouseEvent){
+				MouseEvent mEvent = event.get!(event.Type.MouseEvent);
+				if (mEvent.buttons != MouseEvent.Button.None){
+					MousePosition mPos;
+					mPos.x = mEvent.x;
+					mPos.y = mEvent.y;
+					switch (mEvent.buttons){
+						case MouseEvent.Button.Left:
+							mPos.mouseButton = mPos.Button.Left;
+							break;
+						case MouseEvent.Button.Right:
+							mPos.mouseButton = mPos.Button.Right;
+							break;
+						case MouseEvent.Button.ScrollUp:
+							mPos.mouseButton = mPos.Button.ScrollUp;
+							break;
+						case MouseEvent.Button.ScrollDown:
+							mPos.mouseButton = mPos.Button.ScrollDown;
+							break;
+						default:
+							continue;
+					}
+					this.onClick(mPos);
+				}
+			}else if (event.type == event.Type.SizeChangedEvent){
+				this.update;
+			}
+		}
+	}
+
+	void terminate(){
+		isRunning = false;
+	}
+
+	//override write properties
+	override @property Size size(Size newSize){
+		//don't let anything modify the size
+		return widgetSize;
+	}
+	override @property Position position(Position newPosition){
+		return widgetPosition;
+	}
+
+	//functions below are used by Matrix.flushToTerminal
+	void update(){
+		terminal.flush;
+	}
+	void setColors(RGBColor textColor, RGBColor bgColor){
+		terminal.setTrueColor(textColor, bgColor);
+	}
+	void moveTo(int x, int y){
+		terminal.moveTo(x, y);
+	}
 	void writeChars(char[] c){
 		terminal.write(c);
 	}
-	void setColors(Color textColor, Color bgColor){
-		//terminal.setTrueColor(textColor, bgColor);
+	void writeChars(char c){
+		terminal.write(c);
 	}
-	void update(/*QWidget caller*/){
-		terminal.flush;
-	}
+}
+
+//misc functions:
+private uinteger percentToRaw(uinteger percent, uinteger total){
+	uinteger r;
+	r = cast(uinteger)((cast(float)percent/cast(float)100)*total);
+	return r;
 }
