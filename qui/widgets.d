637a854aa12bce3d3f21d75d8a0cece9f722c2c6
Nafees10 <nafees.hassan@outlook.com>
2017-03-25 14:49:30 +0500

    Finished the progressbar widget, plus fixed a bug related to sizing in TextLabel

--- qui/widgets.d
+++ qui/widgets.d
@@ -25,10 +25,8 @@ public:
 	void onKeyPress(KeyPress key){
 		
 	}
-
 	override @property string caption(string caption){
-		widgetSize.width = caption.length;
-		widgetSize.height = 1;
+		needsUpdate = true;
 		return widgetCaption = caption;
 	}
 }
@@ -38,21 +36,46 @@ private:
 	uinteger max, done;
 	uinteger fillCells;
 	Direction barDirection;
+	RGBColor bgColor, barColor;
 public:
 	enum Direction{
 		Forward, Backward
 	}
-	this(Direction d, uinteger totalAmount, uinteger complete){
+	this(Direction d, uinteger totalAmount = 100, uinteger complete = 0){
 		barDirection = d;
 		max = totalAmount;
-		complete = done;
+		done = complete;
 		fillCells = ratioToRaw(complete, max, widgetSize.width);
 		//set max height
 		widgetSize.maxHeight = 1;
+		//set colors
+		bgColor = hexToColor("000000");
+		barColor = hexToColor("00FF00");
 	}
 	bool update(ref Matrix display){
-		bool r;
-
+		bool r = false;
+		if (needsUpdate){
+			r = true;
+			uinteger filled = ratioToRaw(done, max, widgetSize.width);
+			char[] bar;
+			bar.length = widgetSize.width;
+			if (barDirection == Direction.Forward){
+				bar[0 .. filled] = '-';
+				if (filled < widgetSize.width){
+					bar[filled] = '>';
+					bar[filled+1 .. bar.length] = ' ';
+				}
+			}else if (barDirection == Direction.Backward){
+				uinteger from = bar.length-filled;
+				bar[0 .. from] = ' ';
+				bar[from] = '<';
+				bar[from+1 .. bar.length] = '-';
+			}
+			//write it
+			display.write(bar, barColor, bgColor);
+			needsUpdate = false;
+		}
+		return r;
 	}
 	void onClick(MouseClick mouse){
 		
@@ -75,7 +98,7 @@ public:
 	}
 	@property uinteger progress(uinteger newProgress){
 		needsUpdate = true;
-		fillCells = ratioToRaw(newProgress, newTotal, widgetSize.width);
+		fillCells = ratioToRaw(newProgress, total, widgetSize.width);
 		return done = newProgress;
 	}
 
