b89ac6f5e4487c9556d68041a2f44af5dc81be47
Nafees10 <nafees.hassan@outlook.com>
2017-04-14 20:29:16 +0500

    debug

--- qui/main.d
+++ qui/main.d
@@ -2,20 +2,40 @@
 
 import qui;
 import widgets;
+import core.thread;
+import std.concurrency;
+
+__gshared ProgressbarWidget pBar;
 
 void main(string[] args){
 	TextLabelWidget label = new TextLabelWidget;
 	label.caption = "Caption";
 	label.sizeRatio = 1;
 	QTerminal term = new QTerminal("Title");
-	term.addWidget(label);
 
-	ProgressbarWidget pBar = new ProgressbarWidget(ProgressbarWidget.Direction.Forward, 200, 40);
+	pBar = new ProgressbarWidget(200, 0);
+	pBar.sizeRatio = 1;
+
+	term.addWidget(label);
 	term.addWidget(pBar);
+	pBar.caption = "Potato!";
+
+	spawn(&increment);
 
 	term.run;
 
 	delete term;
 	delete label;
+	delete pBar;
 }
 
+void increment(){
+	while (pBar.progress < pBar.total){
+		Thread.sleep(dur!"msecs"(125));
+		if (pBar.total - pBar.progress < 8){
+			pBar.progress = pBar.total;
+		}else{
+			pBar.progress = pBar.progress + 8;
+		}
+	}
+}
