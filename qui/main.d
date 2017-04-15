879b2159f92866e3f6ba80dc6c8f4576ba512261
Nafees10 <nafees.hassan@outlook.com>
2017-04-15 15:34:10 +0500

    Debug for EditLineWidget

--- qui/main.d
+++ qui/main.d
@@ -18,28 +18,32 @@ void main(string[] args){
 
 	pBar = new ProgressbarWidget(200, 0);
 	pBar.sizeRatio = 1;
-	pBar.caption = "Potato!";
+	pBar.caption = "Progress bar!";
 
+	EditLineWidget edit = new EditLineWidget("Input here: ", "placeholder");
+
+	term.addWidget(edit);
 	term.addWidget(label);
 	term.addWidget(pBar);
 
-	spawn(&increment);
+	//spawn(&increment);
 
 	term.run;
 
 	delete term;
 	delete label;
 	delete pBar;
+	delete edit;
 }
-
+/*
 void increment(){
-	while (pBar.progress < pBar.total){
-		Thread.sleep(dur!"msecs"(125));
+	while (pBar.progress < pBar.total && term.running){
 		if (pBar.total - pBar.progress < 8){
 			pBar.progress = pBar.total;
 		}else{
-			pBar.progress = pBar.progress + 8;
+			pBar.progress = pBar.progress + 2;
 		}
-		term.updateDisplay();
+		//term.updateDisplay();
+		Thread.sleep(dur!"msecs"(50));
 	}
-}
+}*/
