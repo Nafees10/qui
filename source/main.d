d6a766712a742cc837d14aca76bb98460c42426e
Nafees10 <nafees.hassan@outlook.com>
2017-04-22 20:56:06 +0500

    Changed it to a dub package

---
+++ source/main.d
@@ -0,0 +1,22 @@
+﻿module main;
+
+import qui;
+import widgets;
+import core.thread;
+import std.concurrency;
+
+__gshared QTerminal term;
+
+void main(string[] args){
+	term = new QTerminal("Title",QTerminal.LayoutDisplayType.Horizontal);
+
+	ButtonWidget btn = new ButtonWidget();
+	btn.caption = "Click here";
+
+	term.addWidget(btn);
+
+	term.run;
+
+	delete term;
+	delete btn;
+}
