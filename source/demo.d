e953ba35745ff2f9edb2efc6a4c3deaa91f42fa2
Nafees10 <nafeeshassan16@gmail.com>
2019-02-10 12:03:11 +0500

    Updated to test changes
    current result: segfault
    
    Signed-off-by: Nafees10 <nafeeshassan16@gmail.com>

--- source/demo.d
+++ source/demo.d
@@ -13,84 +13,32 @@ version(demo){
 	class App{
 	private:
 		QTerminal term;
-		// to put a border around everything
-		ContainerWidget border;
-		// because we want >1 widgets in terminal, but ContainerWidget allows only one
-		QLayout mainLayout;
-		// just a memo
-		LogWidget logs;
-		// because we want buttons to be in a vertical order, but mainLayout is horizontal
-		QLayout buttonsContainer;
-		// some buttons
-		ButtonWidget incButton, decButton;
-		// just a progress bar
 		ProgressbarWidget progressBar;
+		TextLabelWidget label;
 	public:
 		this(){
-			term = new QTerminal("Dummy Caption", LayoutDisplayType.Horizontal);
-			border = new ContainerWidget;
-			border.margin = 2;
-			border.marginChar = ' ';
-			mainLayout = new QLayout(LayoutDisplayType.Horizontal);
-			
-			logs = new LogWidget();
-			
-			buttonsContainer = new QLayout(LayoutDisplayType.Vertical);
-			incButton = new ButtonWidget("increase");
-			decButton = new ButtonWidget("decrease");
-			
-			progressBar = new ProgressbarWidget(100, 50);
-			
-			// arrange them inside each other
-			// buttons go inside the buttons container, to put them vertically
-			buttonsContainer.addWidget ([incButton, decButton]);
-			// then they go in order: memo:buttons:progressBar
-			mainLayout.addWidget([
-					logs,
-					buttonsContainer,
-					progressBar
-				]);
-			// and the whole container goes inside the form
-			border.widget = mainLayout;
-			term.addWidget(border);
-			
-			// register custom events, to handle button presses
-			incButton.onMouseEvent = &increaseButtonPress;
-			decButton.onMouseEvent = &decreaseButtonPres;
-		}
-		~this(){
-			// destroy them all, or (IDK why) it causes an exception at end
-			.destroy (progressBar);
-			.destroy (decButton);
-			.destroy (incButton);
-			.destroy (buttonsContainer);
-			.destroy (logs);
-			.destroy (mainLayout);
-			.destroy (border);
-			.destroy (term);
+			term = new QTerminal(QLayout.Type.Horizontal);
+			term.addWidget([label, progressBar]);
+			label.caption = "Progress bar: increases/decreases every 1/2 second. 1 2 3 4 5 6 7 8 9 10 9 8 7 6 5 4 3 2 1";
+			progressBar.max = 10;
+			progressBar.progress = 0;
+			progressBar.onTimerEvent = delegate(QWidget caller){
+				static increasing = true;
+				ProgressbarWidget owner = cast(ProgressbarWidget)caller;
+				if (owner.progress >= owner.max){
+					increasing = false;
+				}else if (owner.progress == 0){
+					increasing = true;
+				}
+				if (increasing)
+					owner.progress = owner.progress + 1;
+				else
+					owner.progress = owner.progress -1;
+			};
+			term.registerWidget([label, progressBar]);
 		}
 		void run(){
-			term.run();
-		}
-		/// catch click from incButton
-		void increaseButtonPress(MouseClick mouse){
-			if (progressBar.progress - progressBar.total < 10){
-				progressBar.progress = progressBar.total;
-			}else{
-				progressBar.progress = progressBar.progress + 10;
-			}
-			// add it to log
-			logs.add("incButton pressed");
-		}
-		/// catch click from decButton
-		void decreaseButtonPres(MouseClick mouse){
-			if (progressBar.progress < 10){
-				progressBar.progress = 0;
-			}else{
-				progressBar.progress = progressBar.progress - 10;
-			}
-			// add it to log
-			logs.add("decButton pressed");
+			term.run;
 		}
 	}
 }
