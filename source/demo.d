2965070e2bbb8fba2b3a4aa3283abb9001e087c2
Nafees10 <nafeeshassan16@gmail.com>
2021-09-24 23:11:53 +0500

    updated demo to test scrolling

--- source/demo.d
+++ source/demo.d
@@ -11,11 +11,27 @@ version(demo){
 		QTerminal term = new QTerminal();
 		TextLabelWidget label = new TextLabelWidget("Hello World!");
 		ScrollTestingWidget test = new ScrollTestingWidget(Color.black, Color.white);
+		ScrollContainer sContainer = new ScrollContainer(test);
+		test.height = 100;
 		label.backgroundColor = Color.blue;
-		label.textColor = Color.red;
-		term.fillColor = Color.green;
+		label.textColor = Color.green;
+		term.fillColor = Color.black;
 		label.height = 1;
-		term.addWidget([label, test]);
+		term.addWidget([label, sContainer]);
+		term.onKeyboardEvent = delegate(QWidget, KeyboardEvent key){
+			if (key.isChar && key.state == KeyboardEvent.State.Pressed){
+				if (key.key == 'w')
+					test.scrollY = test.scrollY - 1;
+				else if (key.key == 's')
+					test.scrollY = test.scrollY + 1;
+				else if (key.key == 'a')
+					test.scrollX = test.scrollX - 1;
+				else if (key.key == 'd')
+					test.scrollX = test.scrollX + 1;
+				label.caption = [key.key];
+			}
+			return false;
+		};
 		term.run();
 		.destroy(term); 
 	}
