fb06e46765358ccd37d297c8397fd5d1503b6e69
Nafees Hassan <nafeeshassan16@gmail.com>
2022-05-08 18:59:33 +0500

    Updated demo to test nested scrolling

--- source/demo.d
+++ source/demo.d
@@ -8,40 +8,45 @@ version(demo){
 	import utils.misc : fileToArray;
 	
 	void main (){
-		QTerminal term = new QTerminal();
+		QTerminal term = new QTerminal(QLayout.Type.Horizontal);
+
+		LogWidget log = new LogWidget();
 
 		ScrollContainer mainScroll = new ScrollContainer();
+		mainScroll.scrollOnMouseWheel = true;
+		mainScroll.scrollOnPageUpDown = true;
+		mainScroll.onKeyboardEvent = delegate(QWidget, KeyboardEvent key, bool){
+			log.add("mainScroll->keyboard called");
+			return false;
+		};
+
 		QLayout mainLayout = new QLayout(QLayout.Type.Vertical);
-		mainScroll.setWidget(mainLayout);
-		term.addWidget(mainScroll);
 
-		QLayout titleLayout = new QLayout(QLayout.Type.Horizontal);
-		titleLayout.height = 1;
-		SplitterWidget titleLeft = new SplitterWidget(), titleRight = new SplitterWidget();
-		titleLeft.color = Color.blue; titleRight.color = Color.blue;
-		TextLabelWidget titleLabel = new TextLabelWidget("QUI Demo");
-		titleLabel.backColor = Color.blue;
-		titleLayout.addWidget([titleLeft, titleLabel, titleRight]);
-
-		mainLayout.addWidget(titleLayout);
+		SplitterWidget split = new SplitterWidget();
+		split.color = Color.blue;
 
 		ScrollContainer subScroll = new ScrollContainer();
-		subScroll.height = 40;
-		QLayout scrollTestLayout = new QLayout(QLayout.Type.Horizontal);
-		subScroll.setWidget(scrollTestLayout);
-		scrollTestLayout.height = 50;
-		SplitterWidget leftSpace = new SplitterWidget(), rightSpace = new SplitterWidget();
-		leftSpace.color = Color.green; rightSpace.color = Color.blue;
-		ScrollTestingWidget test = new ScrollTestingWidget();
-		test.width = 100;
-		test.height = 100;
-		scrollTestLayout.addWidget([leftSpace/*, test*/, rightSpace]);
-
-		mainLayout.addWidget(subScroll);
-
-		//mainScroll.scrollOnMouseWheel = true;
 		subScroll.scrollOnPageUpDown = true;
 		subScroll.scrollOnMouseWheel = true;
+		subScroll.onKeyboardEvent = delegate(QWidget, KeyboardEvent key, bool){
+			log.add("subScroll->keyboard called");
+			return false;
+		};
+
+		ScrollTestingWidget subTest = new ScrollTestingWidget();
+		subTest.height = 50;
+		subTest.width = 70;
+
+		subScroll.setWidget(subTest);
+
+		mainLayout.addWidget([split, subScroll]);
+		mainLayout.height = 50;
+		
+		mainScroll.setWidget(mainLayout);
+
+		term.addWidget(mainScroll);
+
+		term.addWidget(log);
 
 		term.run();
 		.destroy(term); 
