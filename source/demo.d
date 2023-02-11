bb3af5263392ad53faf71e54e2211fbe3b438325
Nafees Hassan <nafeeshassan16@gmail.com>
2023-02-11 06:33:10 +0500

    update demo to test QContainer scroll on input

--- source/demo.d
+++ source/demo.d
@@ -12,12 +12,6 @@ version(demo){
 		QTerminal term = new QTerminal;
 		term.widget = new ScrollTestingWidget(true);
 		term.widget.minHeight = 80;
-		term.widget.onKeyboardEvent = (QWidget, KeyboardEvent key, bool cycle){
-			if (key.key == KeyboardEvent.Key.UpArrow && term.scrollY)
-				term.scrollY = term.scrollY - 1;
-			else if (key.key == KeyboardEvent.Key.DownArrow)
-				term.scrollY = term.scrollY + 1;
-		};
 
 		term.run;
 		.destroy(term);
